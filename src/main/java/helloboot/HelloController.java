24d2446d2bbb468080e2106b434de16e02db08b0
changhee <chbong@starlabs.co.kr>
2024-03-06 13:44:51 +0900

    git init

---
+++ src/main/java/helloboot/HelloController.java
@@ -0,0 +1,31 @@
+package helloboot;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+
+@RestController
+public class HelloController  {
+    private final HelloService helloService;
+
+
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+
+
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam("name") String name){
+        if(name == null || name.trim().length() == 0)throw new IllegalArgumentException();
+        return helloService.sayHello(name);
+    }
+
+}
