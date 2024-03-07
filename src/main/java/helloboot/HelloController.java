86a3b5d7ec6d5ea5366b6f55774076310f4841a8
changhee <chbong@starlabs.co.kr>
2024-03-07 16:11:21 +0900

    HelloDecorator 추가

--- src/main/java/helloboot/HelloController.java
+++ src/main/java/helloboot/HelloController.java
@@ -1,25 +1,15 @@
 package helloboot;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
-
 
 @RestController
 public class HelloController  {
     private final HelloService helloService;
 
-
-
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
-
-
     }
 
     @GetMapping("/hello")
--- src/test/java/helloboot/HelloServiceTest.java
+++ src/test/java/helloboot/HelloServiceTest.java
@@ -5,15 +5,19 @@ import org.junit.jupiter.api.Test;
 
 public class HelloServiceTest {
     @Test
-    void simpleHelloService(){
+    void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 
         String ret = helloService.sayHello("Test");
 
         Assertions.assertThat(ret).isEqualTo("Hello Test");
+    }
 
+    @Test
+    void helloDecorator(){
+        HelloDecorator decorator = new HelloDecorator(name -> name);
 
-
-
+        String ret = decorator.sayHello("Test");
+        Assertions.assertThat(ret).isEqualTo("*Test*");
     }
 }
